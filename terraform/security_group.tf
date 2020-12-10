295d26d54cd6e40a590d579893ed5f048b4f528e
hxrxchang <m.y.y.0326.cg@gmail.com>
2020-12-11 00:56:26 +0900

    add(terraform): alb用のsgを作成

--- terraform/security_group.tf
+++ terraform/security_group.tf
@@ -6,3 +6,22 @@ resource "aws_security_group" "ecspresso-example-web" {
     Name = "ecspresso-example-web"
   }
 }
+
+resource "aws_security_group" "ecspresso-example-alb" {
+  name = "ecspresso-example-alb"
+  description = "allow access from internet for alb"
+  vpc_id = aws_vpc.ecspresso_example.id
+  tags = {
+    Name = "ecspresso-example-alb"
+  }
+}
+
+resource "aws_security_group_rule" "ecspresso-example-alb-http" {
+  type = "ingress"
+  from_port = 80
+  to_port   = 80
+  protocol  = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+
+  security_group_id = aws_security_group.ecspresso-example-alb.id
+}
